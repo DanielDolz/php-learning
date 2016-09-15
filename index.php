8008b257f0a93f82bf767a34164cab8b9151d3db
danieldolz <danieldolz@iesebre.com>
2016-09-15 21:35:33 +0200

    variable html

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
-<?php
-
-$greeting = 'Hola';
-
-//echo $greeting;
-
-$name = 'Dani';
-
-echo  "Hola ${name}a!" ;
-
-echo 'Hola ' . $name . '!' ;
-
-echo $greeting . $name . '!' ;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>
+    <?php
+    $greeting="Hola";
+    $name=$_GET['name'];
+    echo $greeting . $name . '!';
+    ?>
+</h1>
+</body>
+</html>
