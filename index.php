c5b01dee600d8084b7bedd8ae3060c29c26befd4
danieldolz <danieldolz@iesebre.com>
2016-09-16 16:01:49 +0200

    arrays1

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-$greeting = "Hola " . $_GET['name'] . "!";
+// $greeting = "Hola " . $_GET['name'] . "!";
+// require 'index.template.php';
 
-require 'index.template.php';
+//ARRAYS o vectors
+//--Estructura de dades--
+
+//$names = array('Sergi', 'Xavi', 'Romeu');
+
+$names = [
+    'Sergi',
+    'Xavi',
+    'Romeu'
+] ;
+
+foreach ($names as $name) {
+    echo "$name</br>";
+}
+
