f587385ea2a15181c1baec8ad36f7e0455ef77c6
danieldolz <danieldolz@iesebre.com>
2016-09-20 17:44:29 +0200

    class crear

--- index.php
+++ index.php
@@ -1,51 +1,16 @@
 <?php
 
-// $greeting = "Hola " . $_GET['name'] . "!";
-// require 'index.template.php';
-
-//*****ARRAYS o vectors*****
-
-//--ESTRUCTURA de dades--
-
-//$names = array('Sergi', 'Xavi', 'Romeu');
-
-//$names = [
-//    'Sergi',
-//    'Xavi',
-//    'Romeu',
-//    45,
-//    true
-//] ;
-//
-////--Arrays ASSOCIATIUS--
-//
-//$person = [
-//    'name' => 'Sergi',
-//    'sn1' => 'Tur',
-//    'sn2' => 'Badenas',
-//    'age' => 38,
-//    'married' => true
-//];
-
-//die('Xivato!');
-//echo "NO S'EXECUTA MAI!";
-//dd();
-//
-////echo $names;
-//var_dump($names);
-//print_r($names);
-
-//echo $names[0];
-//echo $person['sn1'];
-//echo $person['married'];
-
-//copy ('a','b');
-//
-//function hello($name, $sn1){
-//    echo "Hola " . $name . " " . $sn1 . "!";
-//}
 
 require 'functions.php';
 
-require 'index.template.php';
+class Task {
+    public function 
+}
+
+$task = new Task;
+//$task = new Task();
+
+var_dump($task);
+
+//require 'index.template.php';
 
