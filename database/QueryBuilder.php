eedf1808c9b43d6c04bb7197d8a9e234c51a3d90
danieldolz <danieldolz@iesebre.com>
2016-09-20 22:00:51 +0200

    Connexió MySQL: Refactor4

--- database/QueryBuilder.php
+++ database/QueryBuilder.php
@@ -1,5 +1,38 @@
 <?php
 
+// Collaborators - Dependencies
+
+    // ***COL·LABORADORS***
+
+class Contract {
+
+    public $arquitecte;
+    public $paleta;
+    public $lampista;
+
+    /**
+     * Contract constructor.
+     * @param $arquitecte
+     * @param $paleta
+     * @param $lampista
+     */
+
+    public function __construct($arquitecte, $paleta, $lampista)
+    {
+        $this->arquitecte = $arquitecte;
+        $this->paleta = $paleta;
+        $this->lampista = $lampista;
+    }
+
+    public function buildHome($arquitecte, $paleta, $lampista){
+        $this->arquitecte->dissenyar();
+        $this->paleta->construir();
+        $this->lampista->work();
+    }
+
+}
+
+
 
 class QueryBuilder {
 
@@ -16,9 +49,9 @@ class QueryBuilder {
         $this->pdo = $pdo;
     }
 
-    // Collaborators - Dependencies
 
-    // Dependency injection
+
+    // ***Dependency injection***
 
     /**
      * @param $pdo
