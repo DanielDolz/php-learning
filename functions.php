0197e95a23c141b3517095e866d79813c1bcfa7e
danieldolz <danieldolz@iesebre.com>
2016-09-20 21:16:44 +0200

    Connexió MySQL: Refactor1

--- functions.php
+++ functions.php
@@ -18,3 +18,24 @@ function sum() {
 
     return array_sum(func_get_args());
 }
+
+function connect()
+{
+    try {
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova', 'debian-sys-maint', 'taKIgQGMVowkc2aP');
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Error durant la connexió. Motiu: " . $e->getMessage());
+    }
+    return $pdo;
+}
+
+function allTasks($pdo)
+{
+    $query = $pdo->prepare('SELECT * FROM todos');
+
+    $query->execute();
+
+    return $query->fetchAll(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, Task::class);
+
+}
--- index.php
+++ index.php
@@ -5,28 +5,9 @@ require 'functions.php';
 
 require 'Task.php';
 
-//class Task{};
+$pdo = connect();
 
-// PDO: Php Data Objects
-
-//die ("Hola");
-
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=prova','debian-sys-maint','taKIgQGMVowkc2aP');
-// No deuriem posar el pwd -> Tinc problemes x accedir com a root, quan ho resolgue a casa
-//                            ho canviaré
-}
-catch (PDOExceptionException $e) {
-
-    die("Error durant la connexió. Motiu: " . $e->getMessage());
-
-}
-
-$query = $pdo->prepare('SELECT * FROM todos');
-
-$query->execute();
-
-$tasks = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,Task::class);
+$tasks = allTasks($pdo);
 
 require 'index.template.php';
 
