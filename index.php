bd04ffc5b2efd57f28783fc7887bc515331f0513
danieldolz <danieldolz@iesebre.com>
2016-09-20 17:50:24 +0200

    class constructor

--- index.php
+++ index.php
@@ -4,13 +4,26 @@
 require 'functions.php';
 
 class Task {
-    public function 
+
+    public function __construct($a,$b,$c) {
+//        echo "Nova tasca construida";
+        echo $a;
+        echo $b;
+        echo $c;
+    }
+
+
+
+
 }
 
-$task = new Task;
+$task = new Task("ARRI","QUI","TAUN");
+//$task2 = new Task;
+//$task3 = new Task;
+
 //$task = new Task();
 
-var_dump($task);
+//var_dump($task);
 
 //require 'index.template.php';
 
