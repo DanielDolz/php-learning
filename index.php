e04c718bee97410bb99ede3c1f53a91b226118eb
danieldolz <danieldolz@iesebre.com>
2016-09-23 16:28:26 +0200

    router

--- index.php
+++ index.php
@@ -2,20 +2,6 @@
 
 require "core/bootstrap.php";
 require "core/Request.php";
-require 'routes.php';
+require "core/Router.php";
 
-// POO
-
-//HTTP -> Request -> Response
-
-//$request = new Request;
-//$uri = $request->uri();
-
-$uri = Request::uri();
-
-if ( array_key_exists($uri,$routes)) {
-    require $routes[$uri];
-} else {
-    throw new Exception("No s'ha trobat la ruta");
-}
-
+require Router::load('routes.php')->direct(Request::uri());
